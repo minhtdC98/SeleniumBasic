51987768a863b788d6e43ba3dd7f694ee4543e0d
tranduyminh <tranduyminh91@gmail.com>
2025-07-11 14:09:34 +0700

    init

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Init_Driver {
+public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.example.com");
