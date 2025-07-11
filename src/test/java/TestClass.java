728f7065a9d44fa8a94a54db18b3522b39d05d5c
tranduyminh <tranduyminh91@gmail.com>
2025-07-11 14:52:14 +0700

    init

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,13 +2,19 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.model.Status;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
+
 
 public class TestClass {
     WebDriver driver;
@@ -18,14 +24,47 @@ public class TestClass {
         driver = new ChromeDriver();
     }
 
-    @Test
-    public void test() {
+    //    @Test
+    public void explicitWait() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         Allure.step("Go to page");
         driver.get("https://practicetestautomation.com/practice-test-login/");
         driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("student");
         driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("Password123");
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id=\"submit1\"]")));
+        element.click();
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()=\"Log out\"]")).isDisplayed(), "Log out button is not displayed");
+    }
+
+    //    @Test
+    public void implicitWait() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Allure.step("Go to page");
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("student");
+        driver.findElement(By.xpath("//input[@id=\"password1\"]")).sendKeys("Password123");
         driver.findElement(By.xpath("//button[@id=\"submit\"]")).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//a[text()=\"Log out1\"]")).isDisplayed(), "Log out button is not displayed");
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()=\"Log out\"]")).isDisplayed(), "Log out button is not displayed");
+    }
+
+    @Test
+    public void assertIf() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Allure.step("Go to page");
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("incorrectUser");
+        driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("Password123");
+        driver.findElement(By.xpath("//button[@id=\"submit\"]")).click();
+
+        boolean isDisplay = driver.findElement(By.id("error")).isDisplayed();
+        if (isDisplay) {
+            String message = driver.findElement(By.id("error")).getText();
+            Allure.step("Error message: " + message);
+        } else {
+            Allure.step("Error message is not displayed, test failed");
+            Assert.fail("Error message is not displayed");
+        }
+
     }
 
 
